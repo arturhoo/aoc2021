20c939f02470e5583cca831ea4915e1065b81a89
Artur Rodrigues <arturhoo@gmail.com>
2021-12-02 08:17:29 +0000

    2-2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -10,7 +10,7 @@ fn get_reader() -> BufReader<File> {
     return reader;
 }
 
-fn calc_pos() -> (i32, i32) {
+fn calc_pos_simple() -> (i32, i32) {
     let reader = get_reader();
     let mut horiz = 0;
     let mut depth = 0;
@@ -28,7 +28,32 @@ fn calc_pos() -> (i32, i32) {
     (horiz, depth)
 }
 
+fn calc_pos_aim() -> (i32, i32, i32) {
+    let reader = get_reader();
+    let mut horiz = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for line in reader.lines().map(|l| l.unwrap().trim().to_string()) {
+        let v: Vec<&str> = line.split_whitespace().collect();
+        let value: i32 = v[1].parse().expect("expected integer");
+        match v[0] {
+            "forward" => {
+                horiz += value;
+                depth += value * aim;
+            }
+            "down" => aim += value,
+            "up" => aim -= value,
+            _ => println!("Unexpected input: {}", v[0]),
+        }
+    }
+    (horiz, depth, aim)
+}
+
 fn main() {
-    let (horiz, depth) = calc_pos();
+    let (horiz, depth) = calc_pos_simple();
+    println!("{}", horiz * depth);
+
+    let (horiz, depth, _aim) = calc_pos_aim();
     println!("{}", horiz * depth);
 }
