e5ee4c02821f7457e93206a8c3503594a2534f20
Artur Rodrigues <arturhoo@gmail.com>
2021-12-05 13:38:08 +0000

    Refactor d2

--- d2/src/lib.rs
+++ d2/src/lib.rs
@@ -1,39 +1,43 @@
 pub mod util;
 
 pub fn p1(input: &Vec<String>) -> i32 {
-    let mut horiz = 0;
-    let mut depth = 0;
-
-    for line in input {
-        let v: Vec<&str> = line.split_whitespace().collect();
-        let value: i32 = v[1].parse().expect("expected integer");
-        match v[0] {
-            "forward" => horiz += value,
-            "down" => depth += value,
-            "up" => depth -= value,
-            _ => println!("Unexpected input: {}", v[0]),
-        }
-    }
-    horiz * depth
+    let coord = navigate(input, false);
+    coord.horiz * coord.depth
 }
 
 pub fn p2(input: &Vec<String>) -> i32 {
-    let mut horiz = 0;
-    let mut depth = 0;
-    let mut aim = 0;
+    let coord = navigate(input, true);
+    coord.horiz * coord.depth
+}
 
+struct Coord {
+    horiz: i32,
+    depth: i32,
+    aim: i32,
+}
+
+fn navigate(input: &Vec<String>, consider_aim: bool) -> Coord {
+    let mut coord = Coord {
+        horiz: 0,
+        depth: 0,
+        aim: 0,
+    };
     for line in input {
-        let v: Vec<&str> = line.split_whitespace().collect();
-        let value: i32 = v[1].parse().expect("expected integer");
-        match v[0] {
-            "forward" => {
-                horiz += value;
-                depth += value * aim;
+        let tokens: Vec<&str> = line.split_whitespace().collect();
+        let direction = tokens[0];
+        let value: i32 = tokens[1].parse().unwrap();
+        match (direction, consider_aim) {
+            ("forward", false) => coord.horiz += value,
+            ("down", false) => coord.depth += value,
+            ("up", false) => coord.depth -= value,
+            ("forward", true) => {
+                coord.horiz += value;
+                coord.depth += value * coord.aim;
             }
-            "down" => aim += value,
-            "up" => aim -= value,
-            _ => println!("Unexpected input: {}", v[0]),
+            ("down", true) => coord.aim += value,
+            ("up", true) => coord.aim -= value,
+            _ => {}
         }
     }
-    horiz * depth
+    coord
 }
