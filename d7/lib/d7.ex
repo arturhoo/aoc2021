7eefb987ad94a1dfb7cf0a96eb89acb244bb8917
Artur Rodrigues <arturhoo@gmail.com>
2021-12-07 10:22:24 +0000

    7-2

--- d7/lib/d7.ex
+++ d7/lib/d7.ex
@@ -1,20 +1,44 @@
 defmodule D7 do
   def p1(input) do
-    nums =
-      String.split(input)
-      |> Enum.at(0)
-      |> String.split(",")
-      |> Enum.map(fn i -> elem(Integer.parse(i), 0) end)
-
+    nums = parse_input(input)
     median = median(nums)
 
-    Enum.map(nums, fn i -> abs(i - median) end)
+    nums
+    |> Enum.map(&abs(&1 - median))
     |> Enum.sum()
   end
 
-  defp median(list_of_nums) do
-    len = length(list_of_nums)
-    sorted = Enum.sort(list_of_nums)
+  def p2(input) do
+    nums = parse_input(input)
+    avg = avg(nums)
+
+    cost1 =
+      nums
+      |> Enum.map(&triangular_number(abs(&1 - trunc(avg))))
+      |> Enum.sum()
+
+    cost2 =
+      nums
+      |> Enum.map(&triangular_number(abs(&1 - round(avg))))
+      |> Enum.sum()
+
+    if cost1 < cost2 do
+      cost1
+    else
+      cost2
+    end
+  end
+
+  defp parse_input(input) do
+    String.split(input)
+    |> Enum.at(0)
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp median(numbers) do
+    len = length(numbers)
+    sorted = Enum.sort(numbers)
     mid = div(len, 2)
 
     if rem(len, 2) == 0 do
@@ -24,7 +48,14 @@ defmodule D7 do
     end
   end
 
-  def p2(_input) do
-    nil
+  defp avg(numbers) do
+    Enum.sum(numbers) / length(numbers)
+  end
+
+  @doc """
+  https://en.wikipedia.org/wiki/Triangular_number
+  """
+  defp triangular_number(num) do
+    div(Integer.pow(num, 2) + num, 2)
   end
 end
--- d7/test/d7_test.exs
+++ d7/test/d7_test.exs
@@ -9,4 +9,12 @@ defmodule D7Test do
 
     assert D7.p1(input) == 37
   end
+
+  test "part 2" do
+    input = """
+    16,1,2,0,4,2,7,1,2,14
+    """
+
+    assert D7.p2(input) == 168
+  end
 end
