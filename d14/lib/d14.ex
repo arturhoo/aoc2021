9753627770b4b85f4fa71a04432db68de1038f1e
Artur Rodrigues <arturhoo@gmail.com>
2021-12-14 17:22:48 +0000

    Simplify a bit

--- d14/lib/d14.ex
+++ d14/lib/d14.ex
@@ -1,10 +1,10 @@
 defmodule D14 do
   @spec p1(binary) :: number
   def p1(input) do
-    {template, pairs} = parse_input(input)
-    polymer = develop_polymer_naive(template, pairs, 10)
-    freq = Enum.frequencies(polymer)
-    Enum.max(Map.values(freq)) - Enum.min(Map.values(freq))
+    {template, pair_rules} = parse_input(input)
+    polymer = develop_polymer_naive(template, pair_rules, 10)
+    elem_freq = Enum.frequencies(polymer)
+    Enum.max(Map.values(elem_freq)) - Enum.min(Map.values(elem_freq))
   end
 
   @spec p2(binary) :: number
@@ -19,26 +19,23 @@ defmodule D14 do
   end
 
   defp parse_input(input) do
-    [template_str | pairs] = String.split(input, "\n", trim: true)
+    [template_str | pairs_line] = String.split(input, "\n", trim: true)
 
     pairs =
-      pairs
+      pairs_line
       |> Enum.map(&String.split(&1, " -> "))
       |> Map.new(fn [k, v] -> {k, v} end)
 
     {String.codepoints(template_str), pairs}
   end
 
-  defp develop_polymer_naive(polymer, pairs, steps) do
-    Enum.reduce(0..(steps - 1), [polymer], fn _, acc ->
-      [insert_elements(List.first(acc), pairs) | acc]
+  defp develop_polymer_naive(polymer, pair_rules, steps) do
+    Enum.reduce(0..(steps - 1), polymer, fn _, polymer ->
+      insert_elements(polymer, pair_rules)
     end)
-    |> List.first()
   end
 
-  defp insert_elements([e1, e2], pairs) do
-    [e1, pairs[e1 <> e2], e2]
-  end
+  defp insert_elements([e1, e2], pair_rules), do: [e1, pair_rules[e1 <> e2], e2]
 
   defp insert_elements([e1 | [e2 | tail]], pairs) do
     [e1, pairs[e1 <> e2] | insert_elements([e2 | tail], pairs)]
@@ -55,15 +52,19 @@ defmodule D14 do
 
   defp develop_polymer_smart_once(elem_freq, pair_freq, pair_rules) do
     Enum.reduce(pair_freq, {elem_freq, pair_freq}, fn {pair, freq}, {elem_freq, pair_freq} ->
-      new_elem = pair_rules[pair]
-      elem_freq = Map.put(elem_freq, new_elem, Map.get(elem_freq, new_elem, 0) + freq)
-
       [l, r] = String.codepoints(pair)
-      pair_freq = Map.put(pair_freq, pair, pair_freq[pair] - freq)
-      pair_freq = Map.put(pair_freq, l <> new_elem, Map.get(pair_freq, l <> new_elem, 0) + freq)
-      pair_freq = Map.put(pair_freq, new_elem <> r, Map.get(pair_freq, new_elem <> r, 0) + freq)
+      new_elem = pair_rules[pair]
+      elem_freq = increment_key_by(elem_freq, new_elem, freq)
+
+      pair_freq =
+        pair_freq
+        |> increment_key_by(pair, -freq)
+        |> increment_key_by(l <> new_elem, freq)
+        |> increment_key_by(new_elem <> r, freq)
 
       {elem_freq, pair_freq}
     end)
   end
+
+  defp increment_key_by(map, key, x), do: Map.put(map, key, Map.get(map, key, 0) + x)
 end
