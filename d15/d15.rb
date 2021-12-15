079b3dd3f016d675df76c5cfb5c9dff019e73d1c
Artur Rodrigues <arturhoo@gmail.com>
2021-12-15 11:06:13 +0000

    Use dijkstra

--- d15/d15.rb
+++ d15/d15.rb
@@ -9,9 +9,12 @@ class D15
   end
 
   def p1
-    path = [[0, 0]]
-    find_path(path)
-    @best_path_cost
+    @costs = Array.new(@height) { Array.new(@width) { Float::INFINITY } }
+    @costs[0][0] = 0
+    @visited = Set[]
+    @unvisited = (0..@height - 1).map { |h| (0..@width - 1).map { |w| [h, w] } }.flatten(1).to_set
+    dijkstra
+    @costs[@height - 1][@width - 1]
   end
 
   def p2
@@ -26,40 +29,32 @@ class D15
     @height = @grid.length
   end
 
-  def find_path(current_path)
-    last_pos = current_path[-1]
-    current_path_cost = path_cost(current_path)
-    if last_pos[0] == @height - 1 && last_pos[1] == @width - 1 && (@best_path.nil? || current_path_cost < @best_path_cost)
-      @best_path = current_path
-      @best_path_cost = current_path_cost
-      return
-    end
-
-    possible_neigh(last_pos).each do |neigh|
-      unless @best_path_cost && current_path_cost + @grid[neigh[0]][neigh[1]] > @best_path_cost
-        find_path(current_path.dup << neigh)
+  def dijkstra
+    until @unvisited.empty?
+      pos = pick_least_cost_pos
+      @visited.add(pos)
+      unvisited_neigh = possible_neigh(pos).to_set & @unvisited
+      unvisited_neigh.each do |neigh|
+        tentative_cost = @costs[pos[0]][pos[1]] + @grid[neigh[0]][neigh[1]]
+        @costs[neigh[0]][neigh[1]] = tentative_cost if @costs[neigh[0]][neigh[1]] > tentative_cost
       end
     end
-  end
-
-  def path_cost(path)
-    sum = 0
-    path[1..].each do |pos|
-      sum += @grid[pos[0]][pos[1]]
-    end
-    sum
   end
 
   def possible_neigh(pos)
     possible_neigh = []
     possible_neigh << [pos[0] + 1, pos[1]] unless pos[0] == @height - 1
+    possible_neigh << [pos[0] - 1, pos[1]] unless pos[0].zero?
     possible_neigh << [pos[0], pos[1] + 1] unless pos[1] == @width - 1
+    possible_neigh << [pos[0], pos[1] - 1] unless pos[1].zero?
     possible_neigh
   end
-end
 
-class Point
-  attr_accessor :x, :y
+  def pick_least_cost_pos
+    pos = @unvisited.min_by { |p| @costs[p[0]][p[1]] }
+    @unvisited.delete(pos)
+    pos
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
